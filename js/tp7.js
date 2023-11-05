7f4a39bf766f6fdcfe04aca53a9e9619836e8a1d
Leonel Ramella <leonelramella30@gmail.com>
2023-11-05 12:00:47 -0300

    Resuelto

---
+++ js/tp7.js
@@ -0,0 +1,16 @@
+let numero = parseInt(prompt("Indique el número que desea realizar la piramide (no mayor de 50)"));
+
+while(numero > 50 || numero <= 0 || isNaN(numero)){
+    if(confirm("Valor ingresado no valido. ¿Desea intentar nuevamente?")){
+        numero = parseInt(prompt("Indique hasta que número desea realizar la piramide (no mayor de 50)"));
+    } else {
+        break;
+    }
+}
+
+for(let i = numero; i >= 0; i--){
+    for(let j = 1; j <= i; j++){
+        document.write(i);
+    }
+    document.write(`<br>`)
+}
